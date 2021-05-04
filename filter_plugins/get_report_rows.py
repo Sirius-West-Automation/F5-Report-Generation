4b69d55677a9ae81bf814238815774176921e95a
David Matthews <David.Matthews@siriuscom.com>
2021-05-04 14:02:27 -0500

    testing full pass

--- filter_plugins/get_report_rows.py
+++ filter_plugins/get_report_rows.py
@@ -5,10 +5,13 @@ def extract_device_facts(device_facts):
     rows = []
     #pools = device_facts['ltm_pools']['members']
     #monitors = get_monitors_for_server(server_name)
-    ltm_pools = []
+    ltm_pools = device_facts['ltm_pools']
+
 
     for vs in device_facts['virtual_servers']:
         row = {}
+        default_pool = vs['default_pool']
+
         row["server_name"] = vs['name']
         row["destination_address"] = vs['destination_address']
         row["destination_port"] = vs['destination_port']
@@ -16,9 +19,13 @@ def extract_device_facts(device_facts):
         row["enabled"] = vs['enabled']
         row["availability_status"] = vs['availability_status']
         row["status_reason"] = vs['status_reason']
-#        row["monitors"] = vs['monitors']
 
-        rows.append(row)
+        for pool in ltm_pools:
+            if pool['full_path'] == default_pool:
+                for monitor in pool['monitors']:
+                    row["monitor"] = monitor
+                    row["pool_name"] = pool['name']
+                    rows.append(row)
 
     return rows
 
