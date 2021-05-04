f17e22107eba494c6b23f28228adb029236638b9
David Matthews <David.Matthews@siriuscom.com>
2021-05-03 22:34:09 -0500

    now iterating over virtual_servers

--- filter_plugins/get_report_rows.py
+++ filter_plugins/get_report_rows.py
@@ -2,10 +2,13 @@ def get_monitors_for_server(server_name):
     return ""
 
 def extract_device_facts(device_facts):
-    row = {}
     rows = []
+    #pools = device_facts['ltm_pools']['members']
+    #monitors = get_monitors_for_server(server_name)
+    ltm_pools = []
 
     for vs in device_facts['virtual_servers']:
+        row = {}
         row["server_name"] = vs['name']
         row["destination_address"] = vs['destination_address']
         row["destination_port"] = vs['destination_port']
@@ -13,10 +16,11 @@ def extract_device_facts(device_facts):
         row["enabled"] = vs['enabled']
         row["availability_status"] = vs['availability_status']
         row["status_reason"] = vs['status_reason']
-        row["monitors"] = vs['monitors']
+#        row["monitors"] = vs['monitors']
+
         rows.append(row)
 
-    return device_facts.keys()
+    return rows
 
 
 
