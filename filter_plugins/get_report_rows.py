4c756d1348f6a021dd98e33ac9d239dd85030d50
David Matthews <David.Matthews@siriuscom.com>
2021-05-03 22:10:24 -0500

    added virtual_servers data to rows.

--- filter_plugins/get_report_rows.py
+++ filter_plugins/get_report_rows.py
@@ -1,6 +1,31 @@
+def get_monitors_for_server(server_name):
+    return ""
+
 def extract_device_facts(device_facts):
     row = {}
     rows = []
+    pools = device_facts['ltm_pools']['members']
+    server_name = ""
+    destination_address = ""
+    destination_port = ""
+    description = ""
+    enabled = ""
+    availability_status = ""
+    status_reason = ""
+    monitors = get_monitors_for_server(server_name)
+    ltm_pools = []
+
+    for vs in device_facts['virtual_servers']:
+        row["server_name"] = vs['name']
+        row["destination_address"] = vs['destination_address']
+        row["destination_port"] = vs['destination_port']
+        row["description"] = vs['description']
+        row["enabled"] = vs['enabled']
+        row["availability_status"] = vs['availability_status']
+        row["status_reason"] = vs['status_reason']
+        row["monitors"] = vs['monitors']
+        rows.append(row)
+        
     return device_facts.keys()
 
 
